7c8e558ae8e3814e8fe56bda78e840e69dfd9432
郑杰 <zhengjie@tom.com>
2018-10-26 09:17:21 +0800

    修复orgChart插件路径问题

--- src/main/resources/static/plugins/layui.js
+++ src/main/resources/static/plugins/layui.js
@@ -61,7 +61,6 @@
     ,table: 'modules/table' //表格
     ,element: 'modules/element' //常用元素操作
     ,rate: 'modules/rate'  //评分组件
-    ,orgChart: 'modules/orgChart'  //组织结构
     ,carousel: 'modules/carousel' //轮播
     ,flow: 'modules/flow' //流加载
     ,util: 'modules/util' //工具块
